faf625c9adefb63d1255fe6ca7cb5fb0ed7ac9c7
Kian Davoudi-Rad <kiandr@KIAN.hitronhub.home>
2018-10-28 00:30:27 -0700

    review the bash command

--- helloRadware.bat
+++ helloRadware.bat
@@ -1,26 +1,7 @@
 #Kian D.Rad - Dockerizing a web app radware technology interview 
 
-#create-react-app helloradware
-
 # Clone the GitHub repo:  
-git https://github.com/Kiandr/HelloRadware.git
+git clone https://github.com/Kiandr/HelloRadware.git
+cd HelloRadware
 
-
-#Contents of Dockerfile
-
-FROM alpine
-LABEL maintainer="info@kian.co"
-RUN apk add --update nodejs nodejs-npm
-COPY . /src
-WORKDIR /src
-RUN  npm start
-EXPOSE 8080
-ENTRYPOINT ["node", "./app.js"]
-
-# Save the Dockerfile
-docker image build -t radware .
-docker image ls
-
-docker container run -d --name radWareHelloWorld -p 8080:8080 radware
-# show the name of the app 
-docker ps -a
+docker-compose up -d --build
